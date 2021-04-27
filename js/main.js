9e1c571773918c949478bc650570f9c1ca765324
jocantu <jocantu@protonmail.com>
2021-04-26 23:33:07 -0500

    client's slider finished

--- js/main.js
+++ js/main.js
@@ -9,3 +9,16 @@ function myFunction() {
     x.className = "topnav";
   }
 }
+
+/* Location Map */
+var map = L.map('mymap').setView([25.766873, -100.268570],10);
+
+// Open Street Map Tile Layer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+var marker = L.marker([25.766873, -100.268570]).addTo(map);
+
+// Adding pop-up to the marker
+marker.bindPopup('TranspoInova').openPopup();
